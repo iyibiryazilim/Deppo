7518709a2599fbc0084b7826e042098075986892
volkansunuk <volkan.sunuk@iyibiryazilim.com.tr>
2024-08-16 14:46:41 +0300

    add datastore

--- Deppo.Core/Services/ICustomerService.cs
+++ Deppo.Core/Services/ICustomerService.cs
@@ -1,8 +1,13 @@
 using System;
+using Deppo.Core.DataResultModel;
+using Deppo.Core.SortModels;
+using Deppo.Mobile.Core.Models;
 
 namespace Deppo.Core.Services;
 
 public interface ICustomerService
 {
+    Task<DataResult<IEnumerable<Customer>>> GetObjects(HttpClient httpClient,string search, string groupCode, SortModel? orderBy, int page, int pageSize, int firmNumber);
 
+    Task<DataResult<Customer>> GetObjectById(HttpClient httpClient,int ReferenceId, int firmNumber);
 }
--- Deppo.Core/Services/ISupplierService.cs
+++ Deppo.Core/Services/ISupplierService.cs
@@ -1,8 +1,13 @@
 using System;
+using Deppo.Core.DataResultModel;
+using Deppo.Core.Models;
+using Deppo.Core.SortModels;
 
 namespace Deppo.Core.Services;
 
 public interface ISupplierService
 {
+    Task<DataResult<IEnumerable<Supplier>>> GetObjects(HttpClient httpClient,string search, string groupCode, SortModel? orderBy, int page, int pageSize, int firmNumber);
 
+    Task<DataResult<Supplier>> GetObjectById(HttpClient httpClient,int ReferenceId, int firmNumber);
 }
