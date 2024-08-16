4c86180002a4c89f077fca93b00b37923e3cb386
Berkan Serbes <berkan_serbes@hotmail.com>
2024-08-16 08:46:10 +0300

    Add Warehouse datastore and service. Modify warehouse list view and viewmodel

--- Deppo.Core/DataStores/WarehouseDataStore.cs
+++ Deppo.Core/DataStores/WarehouseDataStore.cs
@@ -1,9 +1,74 @@
 using System;
+using System.Net.NetworkInformation;
+using System.Text.Json;
+using Deppo.Core.DataResultModel;
+using Deppo.Core.Models;
 using Deppo.Core.Services;
+using Deppo.Core.SortModels;
 
 namespace Deppo.Core.DataStores;
 
 public class WarehouseDataStore : IWarehouseService
 {
+	string postUrl = $"/gateway/product/" + nameof(Warehouse);
+	public async Task<DataResult<IEnumerable<Warehouse>>> GetObjects(HttpClient httpClient, string search, SortModel? orderBy, int page, int pageSize, int firmNumber)
+	{
+		HttpResponseMessage responseMessage = await httpClient.GetAsync(postUrl + $"?search={search}&orderBy={orderBy}&page={page}&pageSize={pageSize}&firmNumber={firmNumber}");
+		DataResult<IEnumerable<Warehouse>> dataResult = new DataResult<IEnumerable<Warehouse>>();
+		if (responseMessage.IsSuccessStatusCode)
+		{
+			var data = await responseMessage.Content.ReadAsStringAsync();
+			if (data != null)
+			{
+				if (!string.IsNullOrEmpty(data))
+				{
+					var result = JsonSerializer.Deserialize<DataResult<IEnumerable<Warehouse>>>(data, new JsonSerializerOptions
+					{
+						PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+					});
 
+					dataResult.Data = result?.Data;
+					dataResult.IsSuccess = true;
+					dataResult.Message = "success";
+					return dataResult;
+
+				}
+				else
+				{
+					var result = JsonSerializer.Deserialize<DataResult<IEnumerable<Warehouse>>>(data, new JsonSerializerOptions
+					{
+						PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+					});
+
+					dataResult.Data = result?.Data;
+					dataResult.IsSuccess = true;
+					dataResult.Message = "empty";
+					return dataResult;
+				}
+
+			}
+			else
+			{
+				var result = JsonSerializer.Deserialize<DataResult<IEnumerable<Warehouse>>>(data, new JsonSerializerOptions
+				{
+					PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+				});
+
+				dataResult.Data = Enumerable.Empty<Warehouse>();
+				dataResult.IsSuccess = false;
+				dataResult.Message = await responseMessage.Content.ReadAsStringAsync();
+
+				return dataResult;
+			}
+
+
+		}
+		else
+		{
+			dataResult.Data = Enumerable.Empty<Warehouse>();
+			dataResult.IsSuccess = false;
+			dataResult.Message = await responseMessage.Content.ReadAsStringAsync();
+			return dataResult;
+		}
+	}
 }
--- Deppo.Core/Services/IWarehouseService.cs
+++ Deppo.Core/Services/IWarehouseService.cs
@@ -1,8 +1,11 @@
+using Deppo.Core.DataResultModel;
+using Deppo.Core.Models;
+using Deppo.Core.SortModels;
 using System;
 
 namespace Deppo.Core.Services;
 
 public interface IWarehouseService
 {
-
+	Task<DataResult<IEnumerable<Warehouse>>> GetObjects(HttpClient httpClient, string search, SortModel? orderBy, int page, int pageSize, int firmNumber);
 }
