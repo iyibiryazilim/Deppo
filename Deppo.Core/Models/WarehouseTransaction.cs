9eb89dce8304ba539f124cfc9a8384bbbeecb5ea
Berkan Serbes <berkan_serbes@hotmail.com>
2024-08-19 18:07:02 +0300

    fix

---
+++ Deppo.Core/Models/WarehouseTransaction.cs
@@ -0,0 +1,12 @@
+﻿using Deppo.Core.BaseModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deppo.Core.Models;
+
+public class WarehouseTransaction : BaseTransaction
+{
+}
--- Deppo.Mobile.Core/Models/ProductModels/WarehouseDetailModel.cs
+++ Deppo.Mobile.Core/Models/ProductModels/WarehouseDetailModel.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using Deppo.Core.Models;
+using System.Collections.ObjectModel;
 
 namespace Deppo.Mobile.Core.Models.ProductModels;
 
@@ -7,4 +8,12 @@ public partial class WarehouseDetailModel : ObservableObject
 {
 	[ObservableProperty]
 	private Warehouse warehouse = null!;
+
+	[ObservableProperty]
+	private double inputQuantity;
+
+	[ObservableProperty]
+	private double outputQuantity;
+
+	//public ObservableCollection<WarehouseTransaction> LastTransactions { get; } = new();
 }
