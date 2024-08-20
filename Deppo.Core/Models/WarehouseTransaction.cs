de4866ef361695536d5c50fed68d6e1ac6c1ebb5
Berkan Serbes <berkan_serbes@hotmail.com>
2024-08-20 09:11:47 +0300

    fix

--- Deppo.Core/Models/WarehouseTransaction.cs
+++ Deppo.Core/Models/WarehouseTransaction.cs
@@ -1,12 +1,32 @@
 ﻿using Deppo.Core.BaseModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Deppo.Core.Models;
 
 public class WarehouseTransaction : BaseTransaction
 {
+	private int _warehouseNumber;
+	private string _warehouseName = string.Empty;
+
+	public int WarehouseNumber
+	{
+		get => _warehouseNumber;
+		set
+		{
+			if (_warehouseNumber == value) return;
+			_warehouseNumber = value;
+			NotifyPropertyChanged();
+		}
+	}
+
+	public string WarehouseName
+	{
+		get => _warehouseName;
+		set
+		{
+			if (_warehouseName == value) return;
+			_warehouseName = value;
+			NotifyPropertyChanged();
+		}
+	}
+
 }
