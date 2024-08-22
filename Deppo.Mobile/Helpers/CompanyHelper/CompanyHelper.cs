9740a45e355d754a21765c1c031ada6eb86f5470
Berkan Serbes <berkan_serbes@hotmail.com>
2024-08-22 16:38:42 +0300

    CompanyHelper fonksiyonları oluşturuldu

---
+++ Deppo.Mobile/Helpers/CompanyHelper/CompanyHelper.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+
+namespace Deppo.Mobile.Helpers.CompanyHelper;
+
+public static class CompanyHelper
+{
+	public static async Task<int> GetCompanyNumberAsync()
+	{
+		try
+		{
+			var result = await SecureStorage.GetAsync("CompanyNumber");
+
+			if (result is null)
+				return 0;
+
+			return int.Parse(result);
+		}
+		catch(Exception ex)
+		{
+			Debug.WriteLine(ex.Message);
+			throw;
+		}
+	}
+
+	public static async Task<string> GetFormattedCompanyNumber()
+	{
+		try
+		{
+			var result = await SecureStorage.GetAsync("CompanyNumber");
+
+			if (result is null)
+				return string.Empty;
+
+			return result.PadLeft(3, '0');
+		}
+		catch (Exception ex)
+		{
+			Debug.WriteLine(ex.Message);
+			throw;
+		}
+	}
+
+	public static async Task<string> GetCompanyPeriodAsync()
+	{
+		try
+		{
+			var result = await SecureStorage.GetAsync("CompanyPeriod");
+
+			if (result is null)
+				return string.Empty;
+
+			return result.PadLeft(2, '0');
+		}
+		catch(Exception ex)
+		{
+			Debug.WriteLine(ex.Message);
+			throw;
+		}
+	}
+}
