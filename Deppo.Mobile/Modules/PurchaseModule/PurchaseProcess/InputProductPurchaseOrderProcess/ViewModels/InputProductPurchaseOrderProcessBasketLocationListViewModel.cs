b2b577ee5bf22a3708ee08bb9851c472dda123ba
FerdaSedanr <ferdasedanur.mercimek@gmail.com>
2024-09-11 14:28:18 +0300

    InputProductPurchaseOrderProcessBasketLocationListView  ve viewModel eklendi.

---
+++ Deppo.Mobile/Modules/PurchaseModule/PurchaseProcess/InputProductPurchaseOrderProcess/ViewModels/InputProductPurchaseOrderProcessBasketLocationListViewModel.cs
@@ -0,0 +1,452 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Controls.UserDialogs.Maui;
+using Deppo.Core.Services;
+using Deppo.Mobile.Core.Models.BasketModels;
+using Deppo.Mobile.Core.Models.LocationModels;
+using Deppo.Mobile.Core.Models.WarehouseModels;
+using Deppo.Mobile.Helpers.HttpClientHelpers;
+using Deppo.Mobile.Helpers.MappingHelper;
+using Deppo.Mobile.Helpers.MVVMHelper;
+using Deppo.Mobile.Modules.ProductModule.ProductProcess.InputProductProcess.ViewModels;
+using Deppo.Mobile.Modules.ProductModule.ProductProcess.InputProductProcess.Views;
+using DevExpress.Maui.Controls;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deppo.Mobile.Modules.PurchaseModule.PurchaseProcess.InputProductPurchaseOrderProcess.ViewModels;
+
+[QueryProperty(name: nameof(WarehouseModel), queryId: nameof(WarehouseModel))]
+[QueryProperty(name: nameof(InputProductBasketModel), queryId: nameof(InputProductBasketModel))]
+public partial class InputProductPurchaseOrderProcessBasketLocationListViewModel : BaseViewModel
+{
+    private readonly IHttpClientService _httpClientService;
+    private readonly ILocationService _locationService;
+    private readonly IUserDialogs _userDialogs;
+    private readonly IServiceProvider _serviceProvider;
+
+    public InputProductPurchaseOrderProcessBasketLocationListViewModel(IHttpClientService httpClientService,
+        IUserDialogs userDialogs,
+        ILocationService locationService,
+        IServiceProvider serviceProvider)
+    {
+        _httpClientService = httpClientService;
+        _userDialogs = userDialogs;
+        _locationService = locationService;
+        _serviceProvider = serviceProvider;
+
+        ShowLocationsCommand = new Command(async () => await ShowLocationsAsync());
+        PerformSearchCommand = new Command<Entry>(async (searchBar) => await PerformSearchAsync(searchBar));
+        IncreaseCommand = new Command<LocationModel>(async (locationModel) => await IncreaseAsync(locationModel));
+        DecreaseCommand = new Command<LocationModel>(async (locationModel) => await DecreaseAsync(locationModel));
+        ConfirmCommand = new Command(async () => await ConfirmAsync());
+        CancelCommand = new Command(async () => await CancelAsync());
+
+        CloseLocationsCommand = new Command(async () => await CloseLocationsAsync());
+        ItemTappedCommand = new Command<LocationModel>(async (locationModel) => await ItemTappedAsync(locationModel));
+        ConfirmLocationsCommand = new Command(async () => await ConfirmLocationsAsync());
+        LoadItemsCommand = new Command(async () => await LoadItemsAsync());
+        LoadMoreItemsCommand = new Command(async () => await LoadMoreItemsAsync());
+    }
+
+    [ObservableProperty]
+    private WarehouseModel warehouseModel = null!;
+
+    [ObservableProperty]
+    private InputProductBasketModel inputProductBasketModel = null!;
+
+    public ObservableCollection<LocationModel> Items { get; } = new();
+    //public ObservableCollection<LocationModel> SelectedItems { get; } = new();
+
+    [ObservableProperty]
+    private LocationModel selectedItem;
+
+    public Page CurrentPage { get; set; }
+
+    public Command<LocationModel> IncreaseCommand { get; }
+    public Command<LocationModel> DecreaseCommand { get; }
+    public Command<Entry> PerformSearchCommand { get; }
+    public Command ConfirmCommand { get; }
+    public Command CancelCommand { get; }
+
+    #region Location BottomSheet Commands
+
+    public Command LoadItemsCommand { get; }
+    public Command LoadMoreItemsCommand { get; }
+    public Command ShowLocationsCommand { get; }
+    public Command CloseLocationsCommand { get; }
+    public Command<LocationModel> ItemTappedCommand { get; }
+    public Command ConfirmLocationsCommand { get; }
+
+    #endregion Location BottomSheet Commands
+
+    private async Task ShowLocationsAsync()
+    {
+        if (IsBusy)
+            return;
+
+        try
+        {
+            await LoadItemsAsync();
+            CurrentPage.FindByName<BottomSheet>("locationBottomSheet").State = BottomSheetState.HalfExpanded;
+        }
+        catch (Exception ex)
+        {
+            _userDialogs.Alert(ex.Message);
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
+
+    private async Task LoadItemsAsync()
+    {
+        if (IsBusy)
+            return;
+
+        try
+        {
+            IsBusy = true;
+            Items.Clear();
+
+            _userDialogs.ShowLoading("Loading...");
+            await Task.Delay(1000);
+            var httpClient = _httpClientService.GetOrCreateHttpClient();
+
+            var result = await _locationService.GetObjects(
+                httpClient: httpClient,
+                firmNumber: _httpClientService.FirmNumber,
+                periodNumber: _httpClientService.PeriodNumber,
+                warehouseNumber: WarehouseModel.Number,
+                productReferenceId: InputProductBasketModel.ItemReferenceId,
+                skip: 0,
+                take: 20);
+
+            if (result.IsSuccess)
+            {
+                if (result.Data is not null)
+                {
+                    foreach (var item in result.Data)
+                        Items.Add(Mapping.Mapper.Map<LocationModel>(item));
+                }
+            }
+
+            _userDialogs.HideHud();
+        }
+        catch (Exception ex)
+        {
+            _userDialogs.Alert(ex.Message);
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
+
+    private async Task LoadMoreItemsAsync()
+    {
+        if (IsBusy)
+            return;
+
+        try
+        {
+            IsBusy = true;
+
+            _userDialogs.ShowLoading("Loading...");
+            var httpClient = _httpClientService.GetOrCreateHttpClient();
+
+            var result = await _locationService.GetObjects(
+                httpClient: httpClient,
+                firmNumber: _httpClientService.FirmNumber,
+                periodNumber: _httpClientService.PeriodNumber,
+                warehouseNumber: WarehouseModel.Number,
+                productReferenceId: InputProductBasketModel.ItemReferenceId,
+                skip: Items.Count,
+                take: 20);
+
+            if (result.IsSuccess)
+            {
+                if (result.Data is not null)
+                {
+                    foreach (var item in result.Data)
+                        Items.Add(Mapping.Mapper.Map<LocationModel>(item));
+                }
+            }
+
+            _userDialogs.HideHud();
+        }
+        catch (Exception ex)
+        {
+            _userDialogs.Alert(ex.Message);
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
+
+    private async Task CloseLocationsAsync()
+    {
+        if (IsBusy)
+            return;
+
+        try
+        {
+            IsBusy = true;
+
+            _userDialogs.ShowLoading("Loading...");
+            await Task.Delay(500);
+            CurrentPage.FindByName<BottomSheet>("locationBottomSheet").State = BottomSheetState.Hidden;
+            _userDialogs.HideHud();
+        }
+        catch (Exception ex)
+        {
+            _userDialogs.Alert(ex.Message);
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
+
+    private async Task ItemTappedAsync(LocationModel locationModel)
+    {
+        if (IsBusy)
+            return;
+
+        try
+        {
+            IsBusy = true;
+
+            if (locationModel.IsSelected)
+            {
+                Items.FirstOrDefault(x => x.ReferenceId == locationModel.ReferenceId).IsSelected = false;
+            }
+            else
+            {
+                Items.FirstOrDefault(x => x.ReferenceId == locationModel.ReferenceId).IsSelected = true;
+            }
+        }
+        catch (Exception ex)
+        {
+            _userDialogs.Alert(ex.Message);
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
+
+    private async Task ConfirmLocationsAsync()
+    {
+        if (IsBusy)
+            return;
+
+        try
+        {
+            IsBusy = true;
+
+            _userDialogs.ShowLoading("Loading...");
+            await Task.Delay(500);
+            foreach (var item in Items)
+            {
+                if (!InputProductBasketModel.SelectedLocations.Any(x => x.Code == item.Code) && item.IsSelected)
+                {
+                    InputProductBasketModel.SelectedLocations.Add(item);
+                }
+            }
+
+            CurrentPage.FindByName<BottomSheet>("locationBottomSheet").State = BottomSheetState.Hidden;
+            _userDialogs.HideHud();
+        }
+        catch (Exception ex)
+        {
+            _userDialogs.Alert(ex.Message);
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
+
+    private async Task IncreaseAsync(LocationModel locationModel)
+    {
+        if (IsBusy)
+            return;
+
+        try
+        {
+            IsBusy = true;
+
+            SelectedItem = locationModel;
+
+            if (InputProductBasketModel.TrackingType != 0)
+            {
+                await Shell.Current.GoToAsync($"{nameof(InputProductProcessBasketSeriLotListView)}", new Dictionary<string, object>
+                {
+                    [nameof(WarehouseModel)] = WarehouseModel,
+                    [nameof(InputProductBasketModel)] = InputProductBasketModel
+                });
+            }
+            else
+            {
+                locationModel.InputQuantity++;
+            }
+        }
+        catch (Exception ex)
+        {
+            _userDialogs.Alert(ex.Message);
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
+
+    private async Task DecreaseAsync(LocationModel locationModel)
+    {
+        if (IsBusy)
+            return;
+
+        try
+        {
+            IsBusy = true;
+
+            if (locationModel.InputQuantity > 0)
+            {
+                SelectedItem = locationModel;
+
+                if (InputProductBasketModel.TrackingType != 0)
+                {
+                    await Shell.Current.GoToAsync($"{nameof(InputProductProcessBasketSeriLotListView)}", new Dictionary<string, object>
+                    {
+                        [nameof(WarehouseModel)] = WarehouseModel,
+                        [nameof(InputProductBasketModel)] = InputProductBasketModel
+                    });
+                }
+                else
+                {
+                    locationModel.InputQuantity--;
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            _userDialogs.Alert(ex.Message);
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
+
+    private async Task PerformSearchAsync(Entry barcodeEntry)
+    {
+        if (IsBusy)
+            return;
+
+        try
+        {
+            if (string.IsNullOrEmpty(barcodeEntry.Text))
+                return;
+
+            IsBusy = true;
+
+            _userDialogs.ShowLoading("Loading...");
+            await Task.Delay(500);
+
+            var httpClient = _httpClientService.GetOrCreateHttpClient();
+
+            var result = await _locationService.GetObjects(
+                        httpClient: httpClient,
+                        firmNumber: _httpClientService.FirmNumber,
+                        periodNumber: _httpClientService.PeriodNumber,
+                        warehouseNumber: WarehouseModel.Number,
+                        productReferenceId: InputProductBasketModel.ItemReferenceId,
+                        search: barcodeEntry.Text,
+                        skip: 0,
+                        take: 1);
+
+            if (result.IsSuccess)
+            {
+                if (result.Data is not null)
+                {
+                    foreach (var item in result.Data)
+                        InputProductBasketModel.SelectedLocations.Add(Mapping.Mapper.Map<LocationModel>(item));
+
+                    barcodeEntry.Text = string.Empty;
+                    barcodeEntry.Focus();
+                }
+            }
+
+            _userDialogs.HideHud();
+        }
+        catch (Exception ex)
+        {
+            _userDialogs.Alert(ex.Message);
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
+
+    private async Task ConfirmAsync()
+    {
+        if (IsBusy)
+            return;
+
+        try
+        {
+            IsBusy = true;
+
+            _userDialogs.ShowLoading("Loading...");
+
+            var previousViewModel = _serviceProvider.GetRequiredService<InputProductProcessBasketListViewModel>();
+            if (InputProductBasketModel.SelectedLocations.Count > 0)
+            {
+                var totalQuantity = InputProductBasketModel.SelectedLocations.Where(x => x.InputQuantity > 0).Sum(x => (double)x.InputQuantity);
+                previousViewModel.SelectedInputProductBasketModel.Quantity = totalQuantity;
+            }
+
+            await Shell.Current.GoToAsync("..");
+            _userDialogs.HideHud();
+        }
+        catch (Exception ex)
+        {
+            _userDialogs.Alert(ex.Message);
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
+
+    private async Task CancelAsync()
+    {
+        if (IsBusy)
+            return;
+
+        try
+        {
+            IsBusy = true;
+
+            _userDialogs.ShowLoading("Loading...");
+            await Task.Delay(500);
+            await Shell.Current.GoToAsync("..");
+            _userDialogs.HideHud();
+        }
+        catch (Exception ex)
+        {
+            _userDialogs.Alert(ex.Message);
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
+}
