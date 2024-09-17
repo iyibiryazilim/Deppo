a0692f067a934381b1bccf46aaa6121771af18f6
Umut Sönmez <00umutsonmez00@gmail.com>
2024-09-17 11:26:37 +0300

    purchase return dispatch supplierList,dispatchList,productList

--- Deppo.Mobile/Modules/PurchaseModule/PurchaseProcess/ReturnProductPurchaseDispatchProcess/ViewModels/ReturnPurchaseDispatchSupplierListViewModel.cs
+++ Deppo.Mobile/Modules/PurchaseModule/PurchaseProcess/ReturnProductPurchaseDispatchProcess/ViewModels/ReturnPurchaseDispatchSupplierListViewModel.cs
@@ -1,7 +1,196 @@
-﻿using Deppo.Mobile.Helpers.MVVMHelper;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Controls.UserDialogs.Maui;
+using Deppo.Mobile.Core.Models.PurchaseModels;
+using Deppo.Mobile.Core.Models.WarehouseModels;
+using Deppo.Mobile.Core.Services;
+using Deppo.Mobile.Helpers.HttpClientHelpers;
+using Deppo.Mobile.Helpers.MappingHelper;
+using Deppo.Mobile.Helpers.MVVMHelper;
+using Deppo.Mobile.Modules.PurchaseModule.PurchaseProcess.ReturnProductPurchaseDispatchProcess.Views;
+using System.Collections.ObjectModel;
 
 namespace Deppo.Mobile.Modules.PurchaseModule.PurchaseProcess.ReturnProductPurchaseDispatchProcess.ViewModels;
 
+[QueryProperty(name: nameof(WarehouseModel), queryId: nameof(WarehouseModel))]
 public partial class ReturnPurchaseDispatchSupplierListViewModel : BaseViewModel
 {
+    private readonly IHttpClientService _httpClientService;
+    private readonly IUserDialogs _userDialogs;
+    private readonly IPurchaseSupplierService _purchaseSupplierService;
+
+    [ObservableProperty]
+    private WarehouseModel warehouseModel = null!;
+
+    [ObservableProperty]
+    private PurchaseSupplier purchaseSupplier = null!;
+
+    public ReturnPurchaseDispatchSupplierListViewModel(IHttpClientService httpClientService,
+
+    IUserDialogs userDialogs,
+    IPurchaseSupplierService purchaseSupplierService)
+    {
+        _httpClientService = httpClientService;
+        _userDialogs = userDialogs;
+        _purchaseSupplierService = purchaseSupplierService;
+
+        Title = "Tedarikçiler";
+
+        LoadItemsCommand = new Command(async () => await LoadItemsAsync());
+        LoadMoreItemsCommand = new Command(async () => await LoadMoreItemsAsync());
+        ItemTappedCommand = new Command<PurchaseSupplier>(async (x) => await ItemTappedAsync(x));
+
+        NextViewCommand = new Command(async () => await NextViewAsync());
+    }
+
+    public ObservableCollection<PurchaseSupplier> Items { get; } = new();
+
+
+    public Command LoadItemsCommand { get; }
+    public Command LoadMoreItemsCommand { get; }
+    public Command<SearchBar> PerformSearchCommand { get; }
+    public Command ItemTappedCommand { get; }
+    public Command NextViewCommand { get; }
+
+
+    private async Task LoadItemsAsync()
+    {
+        if (IsBusy)
+            return;
+
+        try
+        {
+            IsBusy = true;
+            _userDialogs.ShowLoading("Yükleniyor...");
+            await Task.Delay(1000);
+            Items.Clear();
+
+            var httpClient = _httpClientService.GetOrCreateHttpClient();
+            var result = await _purchaseSupplierService.GetSuppliersWithDispatch(httpClient, _httpClientService.FirmNumber, _httpClientService.PeriodNumber, WarehouseModel.Number, string.Empty, 0, 20);
+            if (result.IsSuccess)
+            {
+                if (result.Data is not null)
+                {
+                    foreach (var item in result.Data)
+                        Items.Add(Mapping.Mapper.Map<PurchaseSupplier>(item));
+
+
+                    _userDialogs.HideHud();
+
+                }
+            }
+
+        }
+        catch (System.Exception ex)
+        {
+            if (_userDialogs.IsHudShowing)
+                _userDialogs.HideHud();
+
+            await _userDialogs.AlertAsync(ex.Message, "Hata", "Tamam");
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
+
+    private async Task LoadMoreItemsAsync()
+    {
+        if (IsBusy)
+            return;
+
+        try
+        {
+            IsBusy = true;
+            _userDialogs.ShowLoading("Yükleniyor...");
+            await Task.Delay(1000);
+
+            var httpClient = _httpClientService.GetOrCreateHttpClient();
+            var result = await _purchaseSupplierService.GetSuppliersWithDispatch(httpClient, _httpClientService.FirmNumber, _httpClientService.PeriodNumber, WarehouseModel.Number, string.Empty, Items.Count, 20);
+            if (result.IsSuccess)
+            {
+                if (result.Data is not null)
+                {
+                    foreach (var item in result.Data)
+                        Items.Add(Mapping.Mapper.Map<PurchaseSupplier>(item));
+
+
+                    _userDialogs.HideHud();
+
+                }
+            }
+
+        }
+        catch (System.Exception ex)
+        {
+            if (_userDialogs.IsHudShowing)
+                _userDialogs.HideHud();
+
+            await _userDialogs.AlertAsync(ex.Message, "Hata", "Tamam");
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
+
+    private async Task ItemTappedAsync(PurchaseSupplier purchaseSupplier)
+    {
+        if (IsBusy)
+            return;
+
+        try
+        {
+            IsBusy = true;
+
+            Items.ToList().ForEach(x => x.IsSelected = false);
+
+            var selectedItem = Items.FirstOrDefault(x => x.ReferenceId == purchaseSupplier.ReferenceId);
+            if (selectedItem is not null)
+            {
+                selectedItem.IsSelected = true;
+                PurchaseSupplier = selectedItem;
+            }
+
+
+        }
+        catch (System.Exception ex)
+        {
+            if (_userDialogs.IsHudShowing)
+                _userDialogs.HideHud();
+
+            await _userDialogs.AlertAsync(ex.Message, "Hata", "Tamam");
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+
+    }
+
+    private async Task NextViewAsync()
+    {
+        if (IsBusy)
+            return;
+
+        try
+        {
+            IsBusy = true;
+
+            await Shell.Current.GoToAsync($"{nameof(ReturnPurchaseDispatchListView)}", new Dictionary<string, object>
+            {
+                [nameof(WarehouseModel)] = WarehouseModel,
+                [nameof(PurchaseSupplier)] = PurchaseSupplier
+            });
+        }
+        catch (System.Exception)
+        {
+
+            throw;
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
+
 }
