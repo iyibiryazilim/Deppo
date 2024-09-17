53f77eb9d8088c0e4a304253d199ccec4633c0fa
Fırat Fatih Demiraran <fatih.demiraran07@gmail.com>
2024-09-17 11:34:54 +0300

    .

--- Deppo.Mobile/Modules/SalesModule/SalesProcess/ReturnProductSalesDispatchProcess/ViewModels/ReturnSalesDispatchListViewModel.cs
+++ Deppo.Mobile/Modules/SalesModule/SalesProcess/ReturnProductSalesDispatchProcess/ViewModels/ReturnSalesDispatchListViewModel.cs
@@ -1,13 +1,27 @@
-﻿using Deppo.Mobile.Helpers.MVVMHelper;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Deppo.Mobile.Core.Models.SalesModels;
+using Deppo.Mobile.Core.Models.WarehouseModels;
+using Deppo.Mobile.Helpers.MVVMHelper;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Deppo.Mobile.Modules.SalesModule.SalesProcess.ReturnProductSalesDispatchProcess.ViewModels
+namespace Deppo.Mobile.Modules.SalesModule.SalesProcess.ReturnProductSalesDispatchProcess.ViewModels;
+//fiş
+[QueryProperty(name: nameof(WarehouseModel), queryId: nameof(WarehouseModel))]
+[QueryProperty(name: nameof(SalesCustomer), queryId: nameof(SalesCustomer))]
+public partial class ReturnSalesDispatchListViewModel : BaseViewModel
 {
-    public partial class ReturnSalesDispatchListViewModel : BaseViewModel
+
+    [ObservableProperty]
+    WarehouseModel warehouseModel = null!;
+
+    [ObservableProperty]
+    SalesCustomer salesCustomer = null!;
+
+    public ReturnSalesDispatchListViewModel()
     {
     }
 }
