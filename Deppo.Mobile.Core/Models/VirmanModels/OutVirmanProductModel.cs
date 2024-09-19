665d31bab7cc3732147c83a99bb507b9eaeec1d1
volkansunuk <volkan.sunuk@iyibiryazilim.com.tr>
2024-09-19 20:33:34 +0300

    VirmanBasketModel added

---
+++ Deppo.Mobile.Core/Models/VirmanModels/OutVirmanProductModel.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.ObjectModel;
+using Android.Database;
+using Deppo.Core.BaseModels;
+using Deppo.Mobile.Core.Models.ProductModels;
+using Deppo.Mobile.Core.Models.SeriLotModels;
+
+namespace Deppo.Mobile.Core.Models.VirmanModels;
+
+public class OutVirmanProductModel : ProductModel
+{
+    public ObservableCollection<SeriLotTransactionModel> SeriLotTransactionModels { get; set; } = new ObservableCollection<SeriLotTransactionModel>();
+}
