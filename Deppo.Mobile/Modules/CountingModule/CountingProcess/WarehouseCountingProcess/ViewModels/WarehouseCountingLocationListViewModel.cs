d42eac13cea7259337b9ac1c9044bcec808f139a
Umut Sönmez <00umutsonmez00@gmail.com>
2024-09-24 17:15:35 +0300

    location

--- Deppo.Mobile/Modules/CountingModule/CountingProcess/WarehouseCountingProcess/ViewModels/WarehouseCountingLocationListViewModel.cs
+++ Deppo.Mobile/Modules/CountingModule/CountingProcess/WarehouseCountingProcess/ViewModels/WarehouseCountingLocationListViewModel.cs
@@ -1,12 +1,188 @@
-﻿using Deppo.Mobile.Helpers.MVVMHelper;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Controls.UserDialogs.Maui;
+using Deppo.Core.Services;
+using Deppo.Mobile.Core.Models.CountingModels;
+using Deppo.Mobile.Core.Models.LocationModels;
+using Deppo.Mobile.Helpers.HttpClientHelpers;
+using Deppo.Mobile.Helpers.MappingHelper;
+using Deppo.Mobile.Helpers.MVVMHelper;
+using Deppo.Mobile.Modules.CountingModule.CountingProcess.WarehouseCountingProcess.Views;
+using System.Collections.ObjectModel;
 
 namespace Deppo.Mobile.Modules.CountingModule.CountingProcess.WarehouseCountingProcess.ViewModels;
 
+[QueryProperty(nameof(WarehouseCountingWarehouseModel), nameof(WarehouseCountingWarehouseModel))]
 public partial class WarehouseCountingLocationListViewModel : BaseViewModel
 {
+    private readonly IHttpClientService _httpClientService;
+    private readonly ILocationService _locationService;
+    private readonly IUserDialogs _userDialogs;
+
+    [ObservableProperty]
+    private LocationModel selectedLocation = null!;
+
+    [ObservableProperty]
+    private WarehouseCountingWarehouseModel warehouseCountingWarehouseModel = null!;
+
+    public ObservableCollection<LocationModel> Items { get; } = new();
+
+    public WarehouseCountingLocationListViewModel(IHttpClientService httpClientService,
+    ILocationService locationService,
+    IUserDialogs userDialogs)
+    {
+        _httpClientService = httpClientService;
+        _locationService = locationService;
+        _userDialogs = userDialogs;
+
+        Title = "Raf Seçimi";
+
+        LoadItemsCommand = new Command(async () => await LoadItemsAsync());
+        LoadMoreItemsCommand = new Command(async () => await LoadMoreItemsAsync());
+        ItemTappedCommand = new Command<LocationModel>(ItemTappedAsync);
+        NextViewCommand = new Command(async () => await NextViewAsync());
+    }
+
+    public Command LoadItemsCommand { get; }
+    public Command LoadMoreItemsCommand { get; }
+    public Command ItemTappedCommand { get; }
+    public Command NextViewCommand { get; }
+
+    private async Task LoadItemsAsync()
+    {
+        if (IsBusy)
+            return;
+
+        try
+        {
+            IsBusy = true;
+
+            _userDialogs.ShowLoading("Loading...");
+            Items.Clear();
+            await Task.Delay(1000);
+            var httpClient = _httpClientService.GetOrCreateHttpClient();
+            var result = await _locationService.GetObjects(httpClient, _httpClientService.FirmNumber, _httpClientService.PeriodNumber, WarehouseCountingWarehouseModel.Number, string.Empty, 0, 20);
+            if (result.IsSuccess)
+            {
+                if (result.Data is not null)
+                {
+                    foreach (var item in result.Data)
+                        Items.Add(Mapping.Mapper.Map<LocationModel>(item));
+                }
+            }
+
+            _userDialogs.HideHud();
+        }
+        catch (Exception ex)
+        {
+            if (_userDialogs.IsHudShowing)
+                _userDialogs.HideHud();
+
+            _userDialogs.Alert(ex.Message);
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
+
+    private async Task LoadMoreItemsAsync()
+    {
+        if (IsBusy)
+            return;
+
+        try
+        {
+            IsBusy = true;
+
+            _userDialogs.ShowLoading("Loading...");
+            var httpClient = _httpClientService.GetOrCreateHttpClient();
+            var result = await _locationService.GetObjects(httpClient, _httpClientService.FirmNumber, _httpClientService.PeriodNumber, WarehouseCountingWarehouseModel.Number, string.Empty, Items.Count, 20);
+            if (result.IsSuccess)
+            {
+                if (result.Data is not null)
+                {
+                    foreach (var item in result.Data)
+                        Items.Add(Mapping.Mapper.Map<LocationModel>(item));
+                }
+            }
+
+            _userDialogs.HideHud();
+        }
+        catch (Exception ex)
+        {
+            if (_userDialogs.IsHudShowing)
+                _userDialogs.HideHud();
+
+            _userDialogs.Alert(ex.Message);
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
+
+    private void ItemTappedAsync(LocationModel item)
+    {
+        if (IsBusy)
+            return;
+
+        try
+        {
+            IsBusy = true;
+
+            if (item == SelectedLocation)
+            {
+                SelectedLocation.IsSelected = false;
+                SelectedLocation = null;
+            }
+            else
+            {
+                if (SelectedLocation != null)
+                {
+                    SelectedLocation.IsSelected = false;
+                }
+
+                SelectedLocation = item;
+                SelectedLocation.IsSelected = true;
+            }
+        }
+        catch (Exception ex)
+        {
+            _userDialogs.Alert(ex.Message);
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
+
+    private async Task NextViewAsync()
+    {
+        if (IsBusy)
+            return;
+
+        try
+        {
+            IsBusy = true;
+
+            if (SelectedLocation is not null)
+            {
+
+                await Shell.Current.GoToAsync($"{nameof(WarehouseCountingBasketView)}", new Dictionary<string, object>
+                {
+                    [nameof(LocationModel)] = SelectedLocation,
+                    [nameof(WarehouseCountingWarehouseModel)] = WarehouseCountingWarehouseModel
+                });
+
+            }
+        }
+        catch (Exception ex)
+        {
+            _userDialogs.Alert(ex.Message);
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
 }
