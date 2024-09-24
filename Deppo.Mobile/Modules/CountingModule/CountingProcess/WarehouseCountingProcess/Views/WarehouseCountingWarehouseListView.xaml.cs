6a5a88ea896f1ea00a2f91e91260298a1eb00a12
Berkan Serbes <berkan_serbes@hotmail.com>
2024-09-24 16:32:21 +0300

    Ambara göre sayım sayfaları eklendi

---
+++ Deppo.Mobile/Modules/CountingModule/CountingProcess/WarehouseCountingProcess/Views/WarehouseCountingWarehouseListView.xaml.cs
@@ -0,0 +1,14 @@
+using Deppo.Mobile.Modules.CountingModule.CountingProcess.WarehouseCountingProcess.ViewModels;
+
+namespace Deppo.Mobile.Modules.CountingModule.CountingProcess.WarehouseCountingProcess.Views;
+
+public partial class WarehouseCountingWarehouseListView : ContentView
+{
+	private readonly WarehouseCountingWarehouseListViewModel _viewModel;
+	public WarehouseCountingWarehouseListView(WarehouseCountingWarehouseListViewModel viewModel)
+	{
+		InitializeComponent();
+		_viewModel = viewModel;
+		BindingContext = _viewModel;
+	}
+}
