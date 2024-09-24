58564decd0b55327ac0ff69036df8675f81ed455
Umut Sönmez <00umutsonmez00@gmail.com>
2024-09-24 11:28:42 +0300

    add queryProperty

--- Deppo.Mobile/Modules/CountingModule/NegativeProductMenu/ViewModels/NegativeProductFormViewModel.cs
+++ Deppo.Mobile/Modules/CountingModule/NegativeProductMenu/ViewModels/NegativeProductFormViewModel.cs
@@ -1,8 +1,18 @@
-﻿using Deppo.Mobile.Helpers.MVVMHelper;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Deppo.Mobile.Core.Models.CountingModels;
+using Deppo.Mobile.Helpers.MVVMHelper;
 
-namespace Deppo.Mobile.Modules.CountingModule.NegativeProductMenu.ViewModels
+namespace Deppo.Mobile.Modules.CountingModule.NegativeProductMenu.ViewModels;
+
+
+[QueryProperty(name: nameof(NegativeProductBasketModel), queryId: nameof(NegativeProductBasketModel))]
+public partial class NegativeProductFormViewModel : BaseViewModel
 {
-    public partial class NegativeProductFormViewModel : BaseViewModel
+    [ObservableProperty]
+    NegativeProductBasketModel negativeProductBasketModel = null!;
+
+    public NegativeProductFormViewModel()
     {
+        
     }
 }
