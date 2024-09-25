51641391a396b005730c9ddb92897901470c1014
Berkan Serbes <berkan_serbes@hotmail.com>
2024-09-25 17:24:56 +0300

    Ürüne göre sayım sayfaları açıldı

---
+++ Deppo.Mobile/Modules/CountingModule/CountingProcess/ProductCountingProcess/Views/ProductCountingProductListView.xaml.cs
@@ -0,0 +1,14 @@
+using Deppo.Mobile.Modules.CountingModule.CountingProcess.ProductCountingProcess.ViewModels;
+
+namespace Deppo.Mobile.Modules.CountingModule.CountingProcess.ProductCountingProcess.Views;
+
+public partial class ProductCountingProductListView : ContentPage
+{
+	private readonly ProductCountingProductListViewModel _viewModel;
+	public ProductCountingProductListView(ProductCountingProductListViewModel viewModel)
+	{
+		InitializeComponent();
+		_viewModel = viewModel;
+		BindingContext = _viewModel;
+	}
+}
