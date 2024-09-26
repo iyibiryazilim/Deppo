1c2cf68b12a4760fa5e92790da61919f6179fe77
FerdaSedanr <ferdasedanur.mercimek@gmail.com>
2024-09-26 16:33:23 +0300

    WorkOrderCalcView Update

--- Deppo.Mobile/Modules/QuicklyProductionModule/QuicklyProductionProcess/WorkOrder/ViewModels/WorkOrderFormViewModel.cs
+++ Deppo.Mobile/Modules/QuicklyProductionModule/QuicklyProductionProcess/WorkOrder/ViewModels/WorkOrderFormViewModel.cs
@@ -1,4 +1,9 @@
-﻿using Deppo.Mobile.Helpers.MVVMHelper;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Controls.UserDialogs.Maui;
+using Deppo.Mobile.Core.Models.QuicklyModels.BasketModels;
+using Deppo.Mobile.Helpers.HttpClientHelpers;
+using Deppo.Mobile.Helpers.MVVMHelper;
+using DevExpress.Maui.Controls;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,11 +12,36 @@ using System.Threading.Tasks;
 
 namespace Deppo.Mobile.Modules.QuicklyProductionModule.QuicklyProductionProcess.WorkOrder.ViewModels
 {
+    [QueryProperty(name: nameof(QuicklyBomProductBasketModel), queryId: nameof(QuicklyBomProductBasketModel))]
     public partial class WorkOrderFormViewModel : BaseViewModel
     {
-        public WorkOrderFormViewModel()
+        private readonly IUserDialogs _userDialogs;
+        private readonly IHttpClientService _httpClientService;
+
+        [ObservableProperty]
+        private QuicklyBomProductBasketModel quicklyBomProductBasketModel = null!;
+
+        [ObservableProperty]
+        private DateTime ficheDate = DateTime.Now;
+
+        [ObservableProperty]
+        private string documentNumber = string.Empty;
+
+        [ObservableProperty]
+        private string documentTrackingNumber = string.Empty;
+
+        [ObservableProperty]
+        private string specialCode = string.Empty;
+
+        [ObservableProperty]
+        private string description = string.Empty;
+
+        public WorkOrderFormViewModel(IHttpClientService httpClientService, IUserDialogs userDialogs)
         {
             Title = "Form Sayfası";
+
+            _httpClientService = httpClientService;
+            _userDialogs = userDialogs;
 
             LoadPageCommand = new Command(async () => await LoadPageAsync());
             ShowBasketItemCommand = new Command(async () => await ShowBasketItemAsync());
@@ -27,16 +57,53 @@ namespace Deppo.Mobile.Modules.QuicklyProductionModule.QuicklyProductionProcess.
 
         public async Task ShowBasketItemAsync()
         {
+            if (IsBusy)
+                return;
+
+            try
+            {
+                IsBusy = true;
+
+                CurrentPage.FindByName<BottomSheet>("basketItemBottomSheet").State = BottomSheetState.HalfExpanded;
+            }
+            catch (Exception ex)
+            {
+                if (_userDialogs.IsHudShowing)
+                    _userDialogs.HideHud();
+
+                await _userDialogs.AlertAsync(ex.Message, "Hata", "Tamam");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
         }
 
         private async Task LoadPageAsync()
         {
+            if (IsBusy)
+                return;
+            try
+            {
+                IsBusy = true;
+
+                CurrentPage.FindByName<BottomSheet>("basketItemBottomSheet").State = BottomSheetState.HalfExpanded;
+            }
+            catch (Exception ex)
+            {
+                if (_userDialogs.IsHudShowing)
+                    _userDialogs.HideHud();
+
+                await _userDialogs.AlertAsync(ex.Message, "Hata", "Tamam");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
         }
 
         private async Task SaveAsync()
         {
         }
-
-
     }
 }
