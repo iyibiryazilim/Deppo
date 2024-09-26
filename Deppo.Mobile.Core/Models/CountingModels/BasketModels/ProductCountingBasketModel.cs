705de67fcd06b5398e50e0933645fef6542a2722
Berkan Serbes <berkan_serbes@hotmail.com>
2024-09-26 09:41:16 +0300

    Add new basketModel

---
+++ Deppo.Mobile.Core/Models/CountingModels/BasketModels/ProductCountingBasketModel.cs
@@ -0,0 +1,91 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Deppo.Mobile.Core.Models.LocationModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deppo.Mobile.Core.Models.CountingModels.BasketModels;
+
+public partial class ProductCountingBasketModel : ObservableObject
+{
+	[ObservableProperty]
+	int productReferenceId;
+
+	[ObservableProperty]
+	string productCode = string.Empty;
+
+	[ObservableProperty]
+	string productName = string.Empty;
+
+	[ObservableProperty]
+	int subUnitsetReferenceId;
+
+	[ObservableProperty]
+	string subUnitsetName = string.Empty;
+
+	[ObservableProperty]
+	string subUnitsetCode = string.Empty;
+
+	[ObservableProperty]
+	int unitsetReferenceId;
+	[ObservableProperty]
+	string unitsetName = string.Empty;
+	[ObservableProperty]
+	string unitsetCode = string.Empty;
+
+	[ObservableProperty]
+	double stockQuantity;
+
+	[ObservableProperty]
+	double outputQuantity;
+
+	[ObservableProperty]
+	string? image;
+
+	[ObservableProperty]
+	bool isVariant;
+
+	[ObservableProperty]
+	int trackingType;
+
+	[ObservableProperty]
+	int locTracking;
+
+	[ObservableProperty]
+	ObservableCollection<LocationTransactionModel> locationTransactions = new();
+
+	[ObservableProperty]
+	double differenceQuantity = 0;
+
+	private bool isCompleted;
+	public bool IsCompleted
+	{
+		get => isCompleted;
+		set
+		{
+			if (SetProperty(ref isCompleted, value))
+			{
+				OnPropertyChanged(nameof(CountingText));
+				OnPropertyChanged(nameof(CountingTextColor));
+			}
+		}
+	}
+
+	private string countingText;
+	public string CountingText
+	{
+		get => IsCompleted ? "Sayıldı" : "Bekliyor";
+		set => SetProperty(ref countingText, value);
+	}
+
+	private string countingTextColor;
+
+	public string CountingTextColor
+	{
+		get => IsCompleted ? "#008000" : "#E6BE0C";
+		set => SetProperty(ref countingTextColor, value);
+	}
+}
