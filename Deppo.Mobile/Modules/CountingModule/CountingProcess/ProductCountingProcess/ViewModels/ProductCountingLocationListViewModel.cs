1459abe405cf72e04414e0ec2fc7013ecd9f2c49
Berkan Serbes <berkan_serbes@hotmail.com>
2024-09-26 10:02:12 +0300

    ProductCountingLocation pages modified

--- Deppo.Mobile/Modules/CountingModule/CountingProcess/ProductCountingProcess/ViewModels/ProductCountingLocationListViewModel.cs
+++ Deppo.Mobile/Modules/CountingModule/CountingProcess/ProductCountingProcess/ViewModels/ProductCountingLocationListViewModel.cs
@@ -1,12 +1,235 @@
-﻿using Deppo.Mobile.Helpers.MVVMHelper;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Controls.UserDialogs.Maui;
+using Deppo.Core.Services;
+using Deppo.Mobile.Core.Models.CountingModels;
+using Deppo.Mobile.Core.Models.LocationModels;
+using Deppo.Mobile.Core.Models.WarehouseModels;
+using Deppo.Mobile.Helpers.HttpClientHelpers;
+using Deppo.Mobile.Helpers.MappingHelper;
+using Deppo.Mobile.Helpers.MVVMHelper;
+using Deppo.Mobile.Modules.CountingModule.CountingProcess.ProductCountingProcess.Views;
+using Deppo.Mobile.Modules.CountingModule.CountingProcess.WarehouseCountingProcess.Views;
+using System.Collections.ObjectModel;
 
 namespace Deppo.Mobile.Modules.CountingModule.CountingProcess.ProductCountingProcess.ViewModels;
 
+
+[QueryProperty(name: nameof(WarehouseTotalModel), queryId: nameof(WarehouseTotalModel))]
 public partial class ProductCountingLocationListViewModel : BaseViewModel
 {
+	private readonly IHttpClientService _httpClientService;
+	private readonly IUserDialogs _userDialogs;
+	private readonly ILocationService _locationService;
+
+	[ObservableProperty]
+	LocationModel selectedLocation = null!;
+
+	[ObservableProperty]
+	WarehouseTotalModel warehouseTotalModel = null!;
+
+	public ObservableCollection<LocationModel> Items { get; } = new();
+	public ProductCountingLocationListViewModel(IHttpClientService httpClientService, IUserDialogs userDialogs, ILocationService locationService)
+	{
+		_httpClientService = httpClientService;
+		_userDialogs = userDialogs;
+		_locationService = locationService;
+
+		Title = "Raf Seçimi";
+
+		LoadItemsCommand = new Command(async () => await LoadItemsAsync());
+		LoadMoreItemsCommand = new Command(async () => await LoadMoreItemsAsync());
+		ItemTappedCommand = new Command<LocationModel>(async (item) => await ItemTappedAsync(item));
+		NextViewCommand = new Command(async () => await NextViewAsync());
+		BackCommand = new Command(async () => await BackAsync());
+
+	}
+
+	public Command LoadItemsCommand { get; }
+	public Command LoadMoreItemsCommand { get; }
+	public Command ItemTappedCommand { get; }
+	public Command NextViewCommand { get; }
+	public Command BackCommand { get; }
+
+	private async Task LoadItemsAsync()
+	{
+		if (IsBusy)
+			return;
+
+		try
+		{
+			IsBusy = true;
+
+			_userDialogs.ShowLoading("Loading...");
+			Items.Clear();
+			await Task.Delay(1000);
+			var httpClient = _httpClientService.GetOrCreateHttpClient();
+			var result = await _locationService.GetObjects(
+				httpClient: httpClient,
+				firmNumber: _httpClientService.FirmNumber,
+				periodNumber: _httpClientService.PeriodNumber,
+				warehouseNumber: WarehouseTotalModel.WarehouseNumber,
+				skip: 0,
+				take: 20
+			);
+
+			if (result.IsSuccess)
+			{
+				if (result.Data is not null)
+				{
+					foreach (var item in result.Data)
+						Items.Add(Mapping.Mapper.Map<LocationModel>(item));
+				}
+			}
+
+			_userDialogs.HideHud();
+		}
+		catch (Exception ex)
+		{
+			if (_userDialogs.IsHudShowing)
+				_userDialogs.HideHud();
+
+			_userDialogs.Alert(ex.Message);
+		}
+		finally
+		{
+			IsBusy = false;
+		}
+	}
+
+	private async Task LoadMoreItemsAsync()
+	{
+		if (IsBusy)
+			return;
+
+		try
+		{
+			IsBusy = true;
+
+			_userDialogs.ShowLoading("Loading More Items...");
+			var httpClient = _httpClientService.GetOrCreateHttpClient();
+			var result = await _locationService.GetObjects(
+				httpClient: httpClient,
+				firmNumber: _httpClientService.FirmNumber,
+				periodNumber: _httpClientService.PeriodNumber,
+				warehouseNumber: WarehouseTotalModel.WarehouseNumber,
+				skip: Items.Count,
+				take: 20
+			);
+
+			if (result.IsSuccess)
+			{
+				if (result.Data is not null)
+				{
+					foreach (var item in result.Data)
+						Items.Add(Mapping.Mapper.Map<LocationModel>(item));
+				}
+			}
+
+			_userDialogs.HideHud();
+		}
+		catch (Exception ex)
+		{
+			if (_userDialogs.IsHudShowing)
+				_userDialogs.HideHud();
+
+			_userDialogs.Alert(ex.Message);
+		}
+		finally
+		{
+			IsBusy = false;
+		}
+	}
+
+	private async Task ItemTappedAsync(LocationModel item)
+	{
+		if (IsBusy)
+			return;
+
+		try
+		{
+			IsBusy = true;
+
+			if (item == SelectedLocation)
+			{
+				SelectedLocation.IsSelected = false;
+				SelectedLocation = null;
+			}
+			else
+			{
+				if (SelectedLocation != null)
+				{
+					SelectedLocation.IsSelected = false;
+				}
+
+				SelectedLocation = item;
+				SelectedLocation.IsSelected = true;
+			}
+		}
+		catch (Exception ex)
+		{
+			if (_userDialogs.IsHudShowing)
+				_userDialogs.HideHud();
+
+			await _userDialogs.AlertAsync(ex.Message, "Hata", "Tamam");
+		}
+		finally
+		{
+			IsBusy = false;
+		}
+	}
+
+	private async Task NextViewAsync()
+	{
+		if (IsBusy)
+			return;
+		try
+		{
+			IsBusy = true;
+
+			if (SelectedLocation is not null)
+			{
+
+				await Shell.Current.GoToAsync($"{nameof(ProductCountingBasketView)}", new Dictionary<string, object>
+				{
+					[nameof(LocationModel)] = SelectedLocation,
+					[nameof(WarehouseTotalModel)] = WarehouseTotalModel
+				});
+
+			}
+		}
+		catch (Exception ex)
+		{
+			if (_userDialogs.IsHudShowing)
+				_userDialogs.HideHud();
+
+			await _userDialogs.AlertAsync(ex.Message, "Hata", "Tamam");
+		}
+		finally
+		{
+			IsBusy = false;
+		}
+	}
+
+	private async Task BackAsync()
+	{
+		if (IsBusy)
+			return;
+		try
+		{
+			IsBusy = true;
+
+			await Shell.Current.GoToAsync("..");
+		}
+		catch (Exception ex)
+		{
+			if (_userDialogs.IsHudShowing)
+				_userDialogs.HideHud();
+
+			await _userDialogs.AlertAsync(ex.Message, "Hata", "Tamam");
+		}
+		finally
+		{
+			IsBusy = false;
+		}
+	}
 }
