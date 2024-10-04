cc2546673d856daeed9a7a1300975ab3a84f8fb2
Umut Sönmez <00umutsonmez00@gmail.com>
2024-10-04 17:07:33 +0300

    Customer and Supplier query fix

--- Deppo.Core/Services/IWaitingPurchaseOrderService.cs
+++ Deppo.Core/Services/IWaitingPurchaseOrderService.cs
@@ -9,5 +9,6 @@ public interface IWaitingPurchaseOrderService
     Task<DataResult<IEnumerable<dynamic>>> GetObjects(HttpClient httpClient, int firmNumber, int periodNumber, string search = "", int skip = 0, int take = 20);
     Task<DataResult<IEnumerable<dynamic>>> GetObjects(HttpClient httpClient, int firmNumber, int periodNumber, int warehouseNumber, string search = "", int skip = 0, int take = 20);
     Task<DataResult<IEnumerable<dynamic>>> GetObjects(HttpClient httpClient, int firmNumber, int periodNumber, int warehouseNumber, int supplierReferenceId, string search = "", int skip = 0, int take = 20);
+    Task<DataResult<IEnumerable<dynamic>>> GetSuppliers(HttpClient httpClient, int firmNumber, int periodNumber, string search = "", int skip = 0, int take = 0);
 
 }
