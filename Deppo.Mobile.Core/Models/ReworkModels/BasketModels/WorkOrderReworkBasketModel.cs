e447976776e6adab5d8d13515d8bf6d6ccffc2bb
Berkan Serbes <berkan_serbes@hotmail.com>
2024-10-10 14:50:25 +0300

    WorkOrderRework

---
+++ Deppo.Mobile.Core/Models/ReworkModels/BasketModels/WorkOrderReworkBasketModel.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Deppo.Mobile.Core.Models.QuicklyModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deppo.Mobile.Core.Models.ReworkModels.BasketModels;
+
+/// <summary>
+///  Used for WorkOrderRework
+/// </summary>
+public partial class WorkOrderReworkBasketModel : ObservableObject
+{
+
+	[ObservableProperty]
+	QuicklyBOMProductModel quicklyBomProduct = null!;
+}
