b2bca9f74e78588dd5ac8735cefcf8c9535ae588
FerdaSedanr <ferdasedanur.mercimek@gmail.com>
2024-10-16 16:28:29 +0300

    .

---
+++ Deppo.Mobile/Modules/OutsourceModule/OutsourceProcess/InputOutsourceProcess/InputOutsourceTransfer/Views/InputOutsourceTransferOutsourceProductListView.xaml.cs
@@ -0,0 +1,15 @@
+using Deppo.Mobile.Modules.OutsourceModule.OutsourceProcess.InputOutsourceProcess.InputOutsourceTransfer.ViewModels;
+
+namespace Deppo.Mobile.Modules.OutsourceModule.OutsourceProcess.InputOutsourceProcess.InputOutsourceTransfer.Views;
+
+public partial class InputOutsourceTransferOutsourceProductListView : ContentPage
+{
+    private readonly InputOutsourceTransferOutsourceProductListViewModel _viewModel;
+
+    public InputOutsourceTransferOutsourceProductListView(InputOutsourceTransferOutsourceProductListViewModel viewModel)
+    {
+        InitializeComponent();
+        _viewModel = viewModel;
+        BindingContext = _viewModel;
+    }
+}
