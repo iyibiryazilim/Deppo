ae7276ec6d9a1cb14d5e86a41e79a47825ec9639
FerdaSedanr <ferdasedanur.mercimek@gmail.com>
2024-12-02 15:04:17 +0300

    customer,product,subunitset service ve datastore eklendi

---
+++ Deppo.Sys.Service/DTOs/ProductDto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deppo.Sys.Service.DTOs;
+
+public class ProductDto
+{
+    public int ReferenceId { get; set; }
+
+    public string Code { get; set; } = string.Empty;
+
+    public string Name { get; set; } = string.Empty;
+
+    public Guid Unitset { get; set; }
+
+    public int UnitsetReferenceId { get; set; }
+
+    public double Vat { get; set; } = default;
+
+    public string Group { get; set; } = string.Empty;
+
+    public string SpeCode { get; set; } = string.Empty;
+
+    public string SpeCOde2 { get; set; } = string.Empty;
+    public string SpeCOde3 { get; set; } = string.Empty;
+    public string SpeCOde4 { get; set; } = string.Empty;
+    public string SpeCOde5 { get; set; } = string.Empty;
+    public int FirmNumber { get; set; }
+
+
+}
---
+++ Deppo.Sys.Service/Services/ISubunitsetService.cs
@@ -0,0 +1,19 @@
+﻿using Deppo.Sys.Service.DTOs;
+using Deppo.Sys.Service.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deppo.Sys.Service.Services;
+
+public interface ISubunitsetService
+{
+    public Task<IEnumerable<SubUnitset>> GetAllAsync(HttpClient httpClient);
+
+    public Task<IEnumerable<SubUnitset>> GetAllAsync(HttpClient httpClient, string filter);
+
+    public Task<SubUnitset> CreateAsync(HttpClient httpClient, SubUnitsetDto dto);
+
+}
