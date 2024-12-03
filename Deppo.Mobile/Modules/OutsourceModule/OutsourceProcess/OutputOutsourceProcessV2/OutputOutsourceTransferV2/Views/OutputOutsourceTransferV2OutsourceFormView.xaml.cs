5fc31675b4085025545d14b5567ce4eb06b65856
FerdaSedanr <ferdasedanur.mercimek@gmail.com>
2024-12-03 12:41:27 +0300

    outsourceTransferV2 update

---
+++ Deppo.Mobile/Modules/OutsourceModule/OutsourceProcess/OutputOutsourceProcessV2/OutputOutsourceTransferV2/Views/OutputOutsourceTransferV2OutsourceFormView.xaml.cs
@@ -0,0 +1,14 @@
+using Deppo.Mobile.Modules.OutsourceModule.OutsourceProcess.OutputOutsourceProcessV2.OutputOutsourceTransferV2.ViewModels;
+
+namespace Deppo.Mobile.Modules.OutsourceModule.OutsourceProcess.OutputOutsourceProcessV2.OutputOutsourceTransferV2.Views;
+
+public partial class OutputOutsourceTransferV2OutsourceFormView : ContentPage
+{
+	private readonly OutputOutsourceTransferV2OutsourceFormViewModel _viewModel;
+    public OutputOutsourceTransferV2OutsourceFormView(OutputOutsourceTransferV2OutsourceFormViewModel viewModel)
+	{
+		InitializeComponent();
+		_viewModel = viewModel;
+		BindingContext = _viewModel;
+	}
+}
