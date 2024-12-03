72a07012746d0a91ecd0cf9f7b6b05fc1ea815ec
Berkan Serbes <berkan_serbes@hotmail.com>
2024-12-03 17:28:04 +0300

    Fason ürünler için datastore ve servis açıldı, fason ürünler sayfası düzenlendi

---
+++ Deppo.Core/Services/IWorkOrderProductService.cs
@@ -0,0 +1,13 @@
+﻿using Deppo.Core.DataResultModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deppo.Core.Services;
+
+public interface IWorkOrderProductService
+{
+	Task<DataResult<IEnumerable<dynamic>>> GetObjects(HttpClient httpClient, int firmNumber, int periodNumber, int warehouseNumber, int currentReferenceId = 0, string search = "", int skip = 0, int take = 20);
+}
