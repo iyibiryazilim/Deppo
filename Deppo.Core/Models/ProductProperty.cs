1c4d20449027ff3de1fd26c0713807101757ac3f
FerdaSedanr <ferdasedanur.mercimek@gmail.com>
2024-12-16 14:02:28 +0300

    productProperty and productpropertyValue query ve data store yazıldı.

--- Deppo.Core/Models/ProductProperty.cs
+++ Deppo.Core/Models/ProductProperty.cs
@@ -4,9 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Deppo.Core.Models
+namespace Deppo.Core.Models;
+public class ProductProperty
 {
-    public class ProductProperty
-    {
-    }
+    public int ReferenceId { get; set; }
+    public string Code { get; set; }=string.Empty;
+    public string Name { get; set; }=string.Empty;
+    public int ProductReferenceId { get; set; }
+    public string ProductCode { get; set; } = string.Empty;
+    public string ProductName { get; set;} = string.Empty;
+    public bool IsActive { get; set; } 
+
 }
--- Deppo.Core/Models/ProductPropertyValue.cs
+++ Deppo.Core/Models/ProductPropertyValue.cs
@@ -4,9 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Deppo.Core.Models
+namespace Deppo.Core.Models;
+
+public class ProductPropertyValue
 {
-    public class ProductPropertyValue
-    {
-    }
+    public int ReferenceId { get; set; }
+    public int ProductReferenceId { get; set; }
+    public string ProductCode { get; set; } = string.Empty;
+    public string ProductName { get; set; } = string.Empty;
+    public int ProductPropertyReferenceId { get; set; }
+    public string ProductPropertyCode { get; set; } = string.Empty;
+    public string ProductPropertyName { get; set; } = string.Empty;
+    public int ValueNumber { get; set; } = default;
+
 }
